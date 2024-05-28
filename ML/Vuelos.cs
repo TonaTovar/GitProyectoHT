67c269feb7cbef0de34a68e9554b36e33c0a5140
Hectortcm98 <hecmejia999@gmail.com>
2024-05-28 10:39:11 -0600

    Creacion de vista Form y GetAll del modelo Vuelo

--- ML/Vuelos.cs
+++ ML/Vuelos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,16 @@ namespace ML
     public  class Vuelos
     {
         public int Id_Vuelo { get; set; }
+
+        [Required(ErrorMessage = "campo requeridoo")]
+        [Display(Name = "Numero de Vuelo")]
         public int Numero_Vuelo { get; set; }
         public string Destino { get; set; }
         public string Origen { get; set;}
+
+        [Display(Name = "Hora de salida")]
         public string Hora_Salida { get; set;}
+        [Display(Name = "Hora de llegada")]
         public string Hora_LLegada { get;set; }
         public ML.Aerolinea aerolinia { get; set; }
         public List<ML.Vuelos> ListVuelos { get; set; }
---
+++ PL/Controllers/VuelosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class VuelosController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            ML.Vuelos vuelos = new ML.Vuelos();
+            return View(vuelos);
+        }
+        [HttpGet]
+        public IActionResult Form(int id_Vuelo)
+        {
+            ML.Vuelos vuelos = new ML.Vuelos();
+            return View(vuelos);
+        }
+    }
+}
