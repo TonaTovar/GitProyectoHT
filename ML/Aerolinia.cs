e0cd75e41c3d1bcc669fb5394589a67c22a66512
Hectortcm98 <hecmejia999@gmail.com>
2024-05-27 18:31:44 -0600

    Nueva rama ML

---
+++ ML/Aerolinia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Aerolinia
+    {
+
+        public int Id_Aerolinia { get; set; }
+
+        public string AeroliniNombre { get; set; }
+    }
+}
---
+++ ML/Vuelos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public  class Vuelos
+    {
+        public int Id_Vuelo { get; set; }
+        public int Numero_Vuelo { get; set; }
+        public string Destino { get; set; }
+
+        public string Origen { get; set;}
+
+        public int Hora_Salida { get; set;}
+        public int Hora_LLegada { get;set; }
+
+        public ML.Aerolinia aerolinia { get; set; }
+    }
+}
