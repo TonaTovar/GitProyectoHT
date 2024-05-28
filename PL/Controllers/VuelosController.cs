c043d6bab02bf6d1de41d006ab77d879ba05a8e9
Tonathiu Cervantes Tovar <cervtona06@gmail.com>
2024-05-28 11:40:28 -0600

    Add Modal and Controller Vuelos

--- PL/Controllers/VuelosController.cs
+++ PL/Controllers/VuelosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PL.Controllers
 {
@@ -7,14 +8,147 @@ namespace PL.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            ML.Vuelos vuelos = new ML.Vuelos();
-            return View(vuelos);
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7240/api/Vuelo/");
+
+                var responseTask = client.GetAsync("GetAll");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    ML.Vuelos vuelos = new ML.Vuelos();
+                    vuelos.aerolinia = new ML.Aerolinea();
+
+                    var result1 = BL.Aerolinea.GetAll();
+                    vuelos.aerolinia.ListAerolineas = result1.Item3;
+
+                    return View(vuelos);
+                }
+                else
+                {
+                    ML.Vuelos vuelos = new ML.Vuelos();
+                    vuelos.aerolinia = new ML.Aerolinea();
+                    return View(vuelos);
+                }
+            }            
         }
         [HttpGet]
-        public IActionResult Form(int id_Vuelo)
+        public IActionResult Form(int IdVuelo)
         {
             ML.Vuelos vuelos = new ML.Vuelos();
-            return View(vuelos);
+            vuelos.aerolinia = new ML.Aerolinea();
+            vuelos.aerolinia.ListAerolineas = new List<ML.Aerolinea>();
+
+            var result1 = BL.Aerolinea.GetAll();
+            vuelos.aerolinia.ListAerolineas = result1.Item3;
+
+            if (IdVuelo != null)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7240/api/Vuelo/");
+
+                    var responseTask = client.GetAsync("GetById?IdVuelo=" + IdVuelo);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return View(vuelos);
+                    }
+                    else
+                    {
+                        return View(vuelos);
+                    }
+                }
+
+            }
+            else
+            {
+                return View(vuelos);
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Form(ML.Vuelos vuelo)
+        {
+            if(vuelo.Id_Vuelo != 0)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7240/api/Vuelo/");
+
+                    var responseTask = client.PostAsJsonAsync<ML.Vuelos>("Update", vuelo);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        ViewBag.Text = "El vuelo se actualizo correctamente";
+                        return PartialView("Modal");
+                    }
+                    else
+                    {
+                        ViewBag.Text = "El vuelo no se actualizo";
+                        return PartialView("Modal");
+                    }
+                }
+            }
+            else
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:7240/api/Vuelo/");
+
+                    var responseTask = client.PostAsJsonAsync<ML.Vuelos>("Add", vuelo);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        ViewBag.Text = "El vuelo se registro correctamente";
+                        return PartialView("Modal");
+                    }
+                    else
+                    {
+                        ViewBag.Text = "El vuelo no se agrego";
+                        return PartialView("Modal");
+                    }
+                }
+            }
+        }
+        public IActionResult Delete(int IdVuelo)
+        {
+            if (IdVuelo != 0)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:7240/api/Vuelo");
+
+                    var responseTask = client.DeleteAsync("Delete?IdVuelo=" + IdVuelo);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        ViewBag.Text = "El vuelo se elimino correctamente";
+                        return PartialView("Modal");
+                    }
+                    else
+                    {
+                        ViewBag.Text = "El vuelo no se elimino";
+                        return PartialView("Modal");
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.Text = "Hubo un error";
+                return PartialView("Modal");
+            }
         }
     }
 }
