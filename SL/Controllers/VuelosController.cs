234b6750ff14ab1772ed54a506eb680e34d5ab49
Hectortcm98 <hecmejia999@gmail.com>
2024-05-28 11:10:38 -0600

    Se realizó la APIS  del Modelo vuelos

---
+++ SL/Controllers/VuelosController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VuelosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/Add")]
+
+        public IActionResult GetAll(ML.Vuelos vuelos)
+        {
+            var result  = BL.Vuelos.GetAll();
+            if (result.Item1)
+            {
+                return Ok(result.Item1);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpPost]
+        [Route("/Add")]
+        public IActionResult Add ([FromBody]ML.Vuelos vuelos)
+        {
+            var result = BL.Vuelos.Add(vuelos);
+            if (result.Item1)
+            {
+                return Ok(result.Item1);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("/GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = BL.Vuelos.GetById(id);
+            if(result.Item1)
+            {
+                return Ok(result.Item1);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [HttpPut]
+        [Route("/Update")]
+        public IActionResult Update(ML.Vuelos vuelos)
+        {
+            var result = BL.Vuelos.Update(vuelos);
+            if (result.Item1)
+            {
+                return Ok(result.Item1);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [HttpDelete]
+        [Route("/Delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = BL.Vuelos.Delete(id);
+            if (result.Item1)
+            {
+                return Ok(result.Item1);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
